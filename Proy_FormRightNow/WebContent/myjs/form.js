5f0b8da03d7d2d61b15a709dee5501094240fadc
WSebastian <asefora2080@gmail.com>
2015-09-18 17:58:50 -0500

    3 commit: arreglando los tipos de documentos

--- Proy_FormRightNow/WebContent/myjs/form.js
+++ Proy_FormRightNow/WebContent/myjs/form.js
@@ -10,9 +10,9 @@ function populateCombo()
 	document.getElementById("cboTipoDocumento").options[0]=new Option("Seleccione", "0");
 
 		document.getElementById("cboTipoDocumento").options[1] = new Option("DNI", "1");
-		document.getElementById("cboTipoDocumento").options[2] = new Option("RUC", "2");
-		document.getElementById("cboTipoDocumento").options[3] = new Option("CRNET EXTRANJERIA", "3");
-		document.getElementById("cboTipoDocumento").options[4] = new Option("PASAPORTE", "4");	
+		document.getElementById("cboTipoDocumento").options[2] = new Option("CE (Carnet Extranjeria)", "2");
+		document.getElementById("cboTipoDocumento").options[3] = new Option("Passport", "3");
+		document.getElementById("cboTipoDocumento").options[4] = new Option("RUC ", "4");	
 }
 
 document.getElementById("cboTipoDocumento").onchange = function()
