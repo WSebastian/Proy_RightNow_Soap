718791ecf1d0ef12feceb5ae18b40401ea90817d
Luis Barboza <lbarboza@gorasac.com>
2015-10-27 13:43:57 -0500

    Generar archivo csv

--- Proy_FormRightNow/src/test/TestQueryCount.java
+++ Proy_FormRightNow/src/test/TestQueryCount.java
@@ -1,8 +1,11 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+
+import com.csvreader.CsvWriter;
 
 
 public class TestQueryCount<T>
@@ -24,13 +27,32 @@ public class TestQueryCount<T>
 		
 		System.out.println("::::::: Tamaño de la lista :::::::: "+incidents.size());
 		
-//		for (int i = 0; i < incidents.size(); i++)
-//		{
-//			
-//			System.out.println(":::::::: "+incidents.get(i));
-//
-//		}
+		String outputFile = "test/export.csv";
+		boolean alreadyExists = new File(outputFile).exists();
+		
+		if(alreadyExists){
+            File ficheroUsuarios = new File(outputFile);
+            ficheroUsuarios.delete();
+        }
+		
+		try{
+			CsvWriter csvOutput = new CsvWriter(new FileWriter(outputFile, true), ',');
+            
+            csvOutput.write("Codigo");
+            csvOutput.write("Nombres");
+            csvOutput.write("Apellidos");
+            csvOutput.write("Correo");
+            csvOutput.endRecord();
+            
+            for (int i = 0; i < incidents.size(); i++)
+    		{
+    			
+    			System.out.println(":::::::: "+incidents.get(i));
 
+    		}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 }
